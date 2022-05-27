e88bd4426b71f2de56fd5e3378dfa1adf9f4f03b
Vestriger <gerashchenko.p@mail.ru>
2022-05-27 15:44:31 +0300

    database

--- project_bedclothes_shop/settings.py
+++ project_bedclothes_shop/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project_bedclothes_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'pashapasha68',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80, db_index=True)
+    slug = models.CharField(max_length=100, db_index=True, unique=True)
+    img = models.ImageField()
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукция'
+
+    def __str__(self):
+        return self.name
